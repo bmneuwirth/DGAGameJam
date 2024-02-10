08eba6d0f2d6cf07b61b999fc0000474b7caaf9b
Ben Neuwirth <bmneuwirth@gmail.com>
2024-02-10 15:30:27 -0500

    take 20 photos and display them in the world

--- Assets/PhotoScript.cs
+++ Assets/PhotoScript.cs
@@ -6,9 +6,13 @@ using UnityEngine;
 public class PhotoScript : MonoBehaviour
 {
     public const int MAX_PHOTOS = 20;
+    public const bool DEBUG_MODE = true;
+    public const float DEBUG_SPEED = 0.01f;
+    public Vector3 DEBUG_CENTER;
 
-    public Material captureMaterial;
+    public GameObject planePrefab;
     public List<RenderTexture> photos;
+    public List<GameObject> photosPlanes;
 
     public int curPhotoIndex = 0;
 
@@ -21,23 +25,45 @@ public class PhotoScript : MonoBehaviour
             RenderTexture rt = new RenderTexture(512, 512, 16, RenderTextureFormat.ARGB32);
             rt.Create();
             photos.Add(rt);
+
+            Vector3 planePos = DEBUG_CENTER;
+            planePos.x = (i - (MAX_PHOTOS / 2)) * 2;
+            GameObject photoPlane = Instantiate(planePrefab, planePos, Quaternion.Euler(90, -90, 90));
+            photosPlanes.Add(photoPlane);
+
+            Material planeMaterial = new Material(Shader.Find("Standard"));
+            photoPlane.GetComponent<MeshRenderer>().material = planeMaterial;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Very basic camera movement for debugging
+        if (DEBUG_MODE)
+        {
+            float xAxisValue = Input.GetAxis("Horizontal");
+            float zAxisValue = Input.GetAxis("Vertical");
+            if (Camera.main != null)
+            {
+                Camera.main.transform.Translate(new Vector3(xAxisValue * DEBUG_SPEED, 0.0f, zAxisValue * DEBUG_SPEED));
+            }
+        }
         if (Input.GetMouseButtonDown(0))
         {
             Debug.Log("Left click pressed");
             RenderTexture curPhoto = photos[curPhotoIndex];
             if (curPhoto && curPhoto.IsCreated())
             {
+                GameObject curPlane = photosPlanes[curPhotoIndex];
+
                 Camera camera = Camera.main;
                 RenderTexture prevTarget = camera.targetTexture;
                 camera.targetTexture = curPhoto;
                 camera.Render();
-                captureMaterial.mainTexture = curPhoto;
+
+                Material captureMat = curPlane.GetComponent<MeshRenderer>().material;
+                captureMat.mainTexture = curPhoto;
                 camera.targetTexture = prevTarget;
                 curPhotoIndex += 1;
                 curPhotoIndex %= MAX_PHOTOS; // Loop through
