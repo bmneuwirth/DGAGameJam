500942b2a2a4fd605e8bec445bd60392a4731b59
chctj21 <zijiac10@gmail.com>
2024-02-11 16:00:05 -0500

    PhotoDisplay

--- Assets/PhotoDisplay.cs
+++ Assets/PhotoDisplay.cs
@@ -1,42 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-using UnityEngine;
-using UnityEngine.UI;
-
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PhotoDisplay : MonoBehaviour
 {
+    public RenderTexture photoTexture; 
     public int slotIndex; 
 
     void Start()
     {
         LoadPhoto();
     }
+
     void LoadPhoto()
     {
-        if (GameManager.instance != null && slotIndex >= 0 && slotIndex < GameManager.instance.capturedPhotos.Length)
+        if (photoTexture != null)
         {
-            RenderTexture photoTexture = GameManager.instance.capturedPhotos[slotIndex];
-            if (photoTexture != null)
-            {
-                Texture2D photoTexture2D = new Texture2D(photoTexture.width, photoTexture.height);
-                RenderTexture.active = photoTexture;
-                photoTexture2D.ReadPixels(new Rect(0, 0, photoTexture.width, photoTexture.height), 0, 0);
-                photoTexture2D.Apply();
-                GetComponent<Image>().sprite = Sprite.Create(photoTexture2D, new Rect(0, 0, photoTexture.width, photoTexture.height), Vector2.one * 0.5f);
-            }
-            else
-            {
-                Debug.LogError("No photo found at slot index " + slotIndex);
-            }
+           
+            Texture2D photoTexture2D = new Texture2D(photoTexture.width, photoTexture.height);
+            RenderTexture.active = photoTexture;
+            photoTexture2D.ReadPixels(new Rect(0, 0, photoTexture.width, photoTexture.height), 0, 0);
+            photoTexture2D.Apply();
+            
+          
+            GetComponent<Image>().sprite = Sprite.Create(photoTexture2D, new Rect(0, 0, photoTexture.width, photoTexture.height), Vector2.one * 0.5f);
         }
         else
         {
-            Debug.LogError("GameManager instance is null or invalid slot index: " + slotIndex);
+            Debug.LogError("No photo texture assigned to the PhotoDisplay component.");
         }
     }
 }
