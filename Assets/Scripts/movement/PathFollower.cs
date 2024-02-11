7acf0bfb4bd790fb99b4670614dcd370ce8300e1
Emily P <emilypenna99@gmail.com>
2024-02-11 14:24:54 -0500

    pathfollowing

---
+++ Assets/Scripts/movement/PathFollower.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFollower : MonoBehaviour
+{
+
+    public Node[] nodes;
+    public GameObject avatar;
+    private int currentNode = 0;
+    public float movementSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         avatar.transform.position = Vector3.MoveTowards(avatar.transform.position, nodes[currentNode].transform.position, movementSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(avatar.transform.position, nodes[currentNode].transform.position) < 0.1f)
+        {
+            currentNode = currentNode + 1;
+            if (currentNode == nodes.Length - 1)
+            {
+                currentNode = 0;
+            }
+        }
+    }
+
+    //void DrawLine()
+    //{
+    //    for (int i = 0; i < nodes.Length; i++)
+    //    {
+    //        if (i == nodes.Length - 1)
+    //        {
+    //            Debug.DrawLine(nodes[i].transform.position, nodes[0].transform.position, Color.green);
+    //        }
+    //        else
+    //            Debug.DrawLine(nodes[i].transform.position, nodes[i + 1].transform.position, Color.green);
+    //    }
+    //}
+}
